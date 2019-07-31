f857ab546a612430765f6bcd137f249b687746ab
Uyen <myuyenn@kpit.com>
2019-07-31 17:09:57 +0530

    Remove shortening

--- CAN Module/usb.py
+++ CAN Module/usb.py
@@ -4,7 +4,7 @@ arduino = serial.Serial('/dev/ttyUSB0',19200, timeout=.1)
 arduino.flushInput()
 while True:
   try:
-     data = arduino.readline()[:-2]
+     data = arduino.readline()
      if data:
         parsed = data.split('\t')
         parsed = list(map(int,parsed[:-1]))
