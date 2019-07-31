e1a6a136deb1dc59befb32e4c0df24844b7c5b53
Uyen <myuyenn@kpit.com>
2019-07-31 17:06:46 +0530

    Add sub comm serial parser

---
+++ CAN Module/usb.py
@@ -0,0 +1,13 @@
+import serial
+
+arduino = serial.Serial('/dev/ttyUSB0',19200, timeout=.1)
+arduino.flushInput()
+while True:
+  try:
+     data = arduino.readline()[:-2]
+     if data:
+        parsed = data.split('\t')
+        parsed = list(map(int,parsed[:-1]))
+        print(parsed)
+  except:
+    print('Error')
